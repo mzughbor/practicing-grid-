82157f96df8a32814efee716a03ec3658063eade
mahmoud <mzughbor@gmail.com>
2025-06-05 11:34:23 +0300

    fc

---
+++ main.js
@@ -0,0 +1,11 @@
+function login(element) {
+    element.innerText = "Log Out";
+}
+
+function add_definition(element) {
+    element.remove();
+}
+
+function like() {
+    alert("Ninja was liked");
+}
